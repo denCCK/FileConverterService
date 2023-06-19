b5e450cd9237e9292fba91a0f58328a0b7c1de0c
Daniil <daniil909514@gmail.com>
2023-06-19 13:39:51 +0300

    Add Json Reader

---
+++ src/main/java/Reader/JsonReader.java
@@ -0,0 +1,67 @@
+package Reader;
+
+import Model.Manufacturer;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class JsonReader {
+    private final String PATH;
+
+    public JsonReader(String path) {
+        this.PATH = path;
+    }
+
+    public ArrayList<Manufacturer> read() throws IOException {
+        ArrayList<Manufacturer> manufacturers = new ArrayList<>();
+
+        String json = Files.readString(Paths.get(PATH));
+        JSONObject file = new JSONObject(json);
+
+        JSONObject manufacturersTag = file.getJSONObject("manufacturers");
+        JSONArray architectureTag = manufacturersTag.getJSONArray("architecture");
+
+        for (int i = 0; i < architectureTag.length(); i++) {
+            JSONObject architecture = architectureTag.getJSONObject(i);
+
+            String architectureName = architecture.getString("name");
+            JSONArray informationAboutManufacturer = architecture.getJSONArray("manufacturer");
+            for (int j = 0; j < informationAboutManufacturer.length(); j++) {
+                JSONObject manufacturer = informationAboutManufacturer.getJSONObject(j);
+
+                String manufacturerName = manufacturer.getString("name");
+                LinkedHashMap<String, Integer> processors = allManufacturerProcessors(manufacturer);
+
+                manufacturers.add(new Manufacturer(architectureName, manufacturerName, processors));
+            }
+        }
+
+        return manufacturers;
+    }
+
+    private LinkedHashMap<String, Integer> allManufacturerProcessors(JSONObject manufacturer) {
+        LinkedHashMap<String, Integer> allManufacturerProcessors = new LinkedHashMap<>();
+        JSONArray processors = manufacturer.getJSONArray("processor");
+        for (int k = 0; k < processors.length(); k++) {
+            JSONObject processor = processors.getJSONObject(k);
+
+
+            String processorName = processor.getString("name");
+            int cores = Integer.parseInt(processor.getString("cores"));
+
+            allManufacturerProcessors.put(processorName, cores);
+        }
+
+        return allManufacturerProcessors;
+    }
+}
